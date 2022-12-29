fcf820986432505d1acb30e07634b370b182ed8f
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-12-28 23:14:57 -0600

    Fix button selector

--- js/layout-control.js
+++ js/layout-control.js
@@ -1,6 +1,6 @@
 const sidebar = document.querySelector('.sidebar');
 const mainContent = document.querySelector('.main-content');
-document.querySelector('#sidebar-button').onclick = function () {
+document.querySelector('#sidebar-btn').onclick = function () {
   sidebar.classList.toggle('sidebar_small');
   mainContent.classList.toggle('main-content_large')
 }
